5662028ca90e6ef7d6e8b18d5adf9ff3eef9f3a6
Bryan Yap <bryan.yap@alliants.com>
2016-11-11 17:26:40 +0000

    use exit for plugin logic

--- src/index.js
+++ src/index.js
@@ -4,34 +4,36 @@ import decache from 'decache'
 export default function babelPluginInlineJsonImports({ types: t }) {
   return {
     visitor: {
-      ImportDeclaration(path, state) {
-        const { node } = path
+      ImportDeclaration: {
+        exit(path, state) {
+          const { node } = path
 
-        const moduleName = node.source.value
+          const moduleName = node.source.value
 
-        if (moduleName.match(/\.json$/)) {
-          const variableName = node.specifiers[0].local.name
+          if (moduleName.match(/\.json$/)) {
+            const variableName = node.specifiers[0].local.name
 
-          const fileLocation = state.file.opts.filename
-          let filepath = null
+            const fileLocation = state.file.opts.filename
+            let filepath = null
 
-          if (fileLocation === 'unknown') {
-            filepath = moduleName
-          } else {
-            filepath = Path.join(Path.resolve(fileLocation), '..', moduleName)
+            if (fileLocation === 'unknown') {
+              filepath = moduleName
+            } else {
+              filepath = Path.join(Path.resolve(fileLocation), '..', moduleName)
+            }
+
+            const json = requireFresh(filepath)
+
+            path.replaceWith(
+              t.variableDeclaration('const', [
+                t.variableDeclarator(
+                  t.identifier(variableName),
+                  t.valueToNode(json),
+                ),
+              ])
+            )
           }
-
-          const json = requireFresh(filepath)
-
-          path.replaceWith(
-            t.variableDeclaration('const', [
-              t.variableDeclarator(
-                t.identifier(variableName),
-                t.valueToNode(json),
-              ),
-            ])
-          )
-        }
+        },
       },
     },
   }
